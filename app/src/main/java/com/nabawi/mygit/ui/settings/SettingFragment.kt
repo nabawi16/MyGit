323f32e8bae3c6eb8e95a787085d2133ba49ef5a
nabawi16 <a.nabawi16@gmail.com>
2024-03-21 18:42:17 +0700

    reset project

--- app/src/main/java/com/nabawi/mygit/ui/settings/SettingFragment.kt
+++ app/src/main/java/com/nabawi/mygit/ui/settings/SettingFragment.kt
@@ -13,7 +13,6 @@ class SettingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
